bcd7e0cc28b4669bbcfebe824c70e675f8c40d4e
Jayaprakash Parapatla <JayaprakashReddyAz@outlook.com>
2025-06-24 17:30:27 +0000

    Updated LoginForm.jsx

--- frontend/PeopleTrackerFrontEnd/src/components/LoginForm.jsx
+++ frontend/PeopleTrackerFrontEnd/src/components/LoginForm.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
-
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
