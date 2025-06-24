fb2316de936f0ac0624234f2c7b49567bbafedc2
BhargaviNallam21 <bhargavi.nallam21@gmail.com>
2025-06-24 13:07:57 -0400

    added frontend code to github

--- frontend/PeopleTrackerFrontEnd/src/components/LoginForm.jsx
+++ frontend/PeopleTrackerFrontEnd/src/components/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
+
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
