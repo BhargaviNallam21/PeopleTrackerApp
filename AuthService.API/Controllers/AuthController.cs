2c7530afcc55952da1031a84e5539fe8e89987f5
Jayaprakash Parapatla <JayaprakashReddyAz@outlook.com>
2025-06-24 17:32:40 +0000

    Updated AuthController.cs

--- AuthService.API/Controllers/AuthController.cs
+++ AuthService.API/Controllers/AuthController.cs
@@ -8,6 +8,7 @@ namespace AuthService.API.Controllers
     [ApiController]
     [Route("api/[controller]")]
 
+
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
